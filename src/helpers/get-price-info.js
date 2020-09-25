02370fbcb338b2fc9ed53aa4623f07b7c83f8e1c
Roman Privalov <smwb.tech@gmail.com>
2020-09-25 15:55:55 +0300

    Create get-price-info.js
    *Added: function to get total price of order

---
+++ src/helpers/get-price-info.js
@@ -0,0 +1,19 @@
+/**
+ * Получение общей стоимости заказа
+ * @returns {(boolean<false> | number)} - возвращает цену float или false
+ */
+export default function () {
+	const totalPriceSelector = '.shortgood_body .shortgood_total';
+	const totalPriceElement = document.querySelector(totalPriceSelector);
+	if (totalPriceElement) {
+		const priceText = totalPriceElement.innerText
+			?.split('\n')?.[2]
+			?.replace(/ /g, '');
+		if (priceText) {
+			const priceFloat = parseFloat(priceText);
+			return priceFloat;
+		}
+		return false;
+	}
+	return false;
+}
